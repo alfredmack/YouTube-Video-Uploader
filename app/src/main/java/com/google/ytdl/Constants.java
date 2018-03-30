f48f9d86ecee44e94a71403cc1b65f8ed89a2b44
Alfred Mack <alfredmack@gmail.com>
2018-03-30 13:14:14 +0300

    Updated
    Update to API 27

--- app/src/main/java/com/google/ytdl/Constants.java
+++ app/src/main/java/com/google/ytdl/Constants.java
@@ -24,6 +24,6 @@ public class Constants {
     public static final String DEFAULT_KEYWORD = "ytdl";
     // A playlist ID is a string that begins with PL. You must replace this string with the correct
     // playlist ID for the app to work
-    public static final String UPLOAD_PLAYLIST = "Replace me with the playlist ID you want to upload into";
+    public static final String UPLOAD_PLAYLIST = "PLmikGb5Ws7oPnhxisajQJYqHPyOY_UOea";
     public static final String APP_NAME = "ytd-android";
 }
